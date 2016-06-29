7a8144a33ed40a853984a1ccb8ea51e9e912a48a
Ranjandas A P <thejranjan@gmail.com>
2016-06-29 15:20:04 +1000

    Added Tini Init to containers. Modified entrypoint script to test connectivity before starting services.

--- agent/files/entrypoint.sh
+++ agent/files/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+while ! bash -c "echo > /dev/tcp/${CLOUDERA_MANAGER}/7182"; do sleep 1; done
+
 service cloudera-scm-agent start
 
 tail -F /var/log/cloudera-scm-agent/cloudera-scm-agent.log
--- manager/files/entrypoint.sh
+++ manager/files/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+while ! bash -c "echo > /dev/tcp/${MYSQL_SERVER}/3306"; do sleep 1; done
+
 service cloudera-scm-server start
 
 tail -F /var/log/cloudera-scm-server/cloudera-scm-server.log
